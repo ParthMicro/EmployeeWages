a1d7c4653060204afac216c1e5e2f718a1c76a31
ParthMicro <goyalparth058@gmail.com>
2025-03-21 12:03:49 +0530

    Storing day, Hrs worked and Wage earned in a single object

--- EmployeePresence.js
+++ EmployeePresence.js
@@ -192,18 +192,39 @@ const findTotal = (totalVal, dailyVal) => {
 let count =0;
 let totalHours = Array.from(empDailyHrsMap.values()).reduce(findTotal, 0);
 let totalSalary = empDailyWageArr.filter(dailyWage => dailyWage > 0).reduce(findTotal, 0);
-    console.log("UC9A — Emp Wage with Arrow. : "+ " Total Hours: "+ totalHours +
+console.log("UC9A — Emp Wage with Arrow. : "+ " Total Hours: "+ totalHours +
     " Total Wages: "+ totalSalary);
-    console.log("Emp Hrs Map: ");
-    console.log(empDailyHrsMap)
-    let nonWorkingDays = new Array();
-    let partWorkingDays = new Array();
-    let fullWorkingDays = new Array();
-    empDailyHrsMap.forEach((value, key, map) =>{
-        if (value==8) fullWorkingDays.push(key);
-        else if (value==4) partWorkingDays.push(key);
-        else nonWorkingDays.push(key) ;
-    });
-    console.log("Full Working Days: "+fullWorkingDays);
-    console.log("Part Working Days: "+partWorkingDays);
-    console.log("Non Working Days: "+nonWorkingDays);
+console.log("Emp Hrs Map: ");
+console.log(empDailyHrsMap)
+let nonWorkingDays = new Array();
+let partWorkingDays = new Array();
+let fullWorkingDays = new Array();
+empDailyHrsMap.forEach((value, key, map) =>{
+    if (value==8) fullWorkingDays.push(key);
+    else if (value==4) partWorkingDays.push(key);
+    else nonWorkingDays.push(key) ;
+});
+console.log("Full Working Days: "+fullWorkingDays);
+console.log("Part Working Days: "+partWorkingDays);
+console.log("Non Working Days: "+nonWorkingDays);
+
+//UC10
+totalEmpHrs =0;
+totalWorkingDays =0;
+let empDailyHrsAndWageArr = new Array();
+while(totalEmpHrs <=MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS){
+    totalWorkingDays++;
+    let empCheck = Math. floor (Math. random() * 10)%3;
+    let empHrs = getWorkingHours(empCheck);
+    totalEmpHrs +=empHrs;
+    empDailyHrsAndWageArr.push({
+        dayNum:totalWorkingDays,
+        dailyHours : empHrs,
+        dailyWage :calcDailyWage(empHrs),
+        toString(){
+            return '\nDay' +this.dayNum + ' => Working Hours is '+ this.dailyHours
+            + 'And Wage Earned is ' + this.dailyWage
+        },
+    })
+}
+console.log("UC10 Showing Daily Hours Worked and Wage Earned " + empDailyHrsAndWageArr)
